c973289e63666b9295203b93b703f09ac138cbca
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2023-02-20 00:03:42 +0700

    Update signup.js

--- sys-auth/assets/src/signup.js
+++ sys-auth/assets/src/signup.js
@@ -3,14 +3,14 @@ function updateDomainTypeView(e) {
     if (e.target.id == 'i_domain_type_own') {
         getElement('section_custom_domain').classList.remove("hidden");
         getElement('section_subdomain').classList.add("hidden");
-        getElement('input_domain').focus();
+        getElement('input_custom_domain').focus();
     } else {
         getElement('section_subdomain').classList.remove("hidden");
         getElement('section_custom_domain').classList.add("hidden");
         getElement('input_subdomain').focus();
     }
 }
-document.querySelectorAll("input[name='i_domain_type']").forEach((input) => {
+document.querySelectorAll("input[name='domain_type']").forEach((input) => {
     input.addEventListener('change', updateDomainTypeView);
 });
 
