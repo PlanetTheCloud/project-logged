66faa68568761deb505c669c4c83bf8215900b3f
Loading.. Please Wait <eatwatermaln@gmail.com>
2023-04-30 10:47:08 +0800

    PHP 8.1 - PHP Deprecated: trim() - Fixes

---
+++ sys-auth/app/bootstrap.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * Project LOGGED v2.0
+ * Bootstrap File
+ * ---
+ * No modification is needed.
+ * Read the documentation.
+ */
+
+# Prevent direct file access
+if (strpos($_SERVER['REQUEST_URI'], 'sys-auth') !== false) {
+    die(header('HTTP/1.1 403 Forbidden'));
+}
+
+# Definitions
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('SYSTEM', ROOT . '/sys-auth');
+define('PAGES', SYSTEM . '/pages');
+define('APP', SYSTEM . '/app');
+define('IS_API_REQUEST', strpos($_SERVER['REQUEST_URI'], '/api/') !== false);
+
+# Load classes
+$classes = glob(SYSTEM . "/app/classes/*.php");
+foreach ($classes as $class) {
+    require $class;
+}
+$exception_classes = glob(SYSTEM . "/app/classes/Exceptions/*.php");
+foreach ($exception_classes as $class) {
+    require $class;
+}
+unset($classes, $exception_classes);
+
+# Load System config
+define('SYSTEM_CONFIG', Arr::dot(require SYSTEM . '/config/system.php'));
+
+# Response function for API requests
+function apiErrorResponse(string $message = null, array $data = [], bool $merge_data = false)
+{
+    if ($data !== [] && $merge_data) {
+        $toMerge = ['details' => $data];
+    } else if ($data !== [] && SYSTEM_CONFIG['development_mode']) {
+        $toMerge = ['dev_details' => $data];
+    }
+    $message = (!empty(trim($message ?? ""))) ? $message : null;
+    if (SYSTEM_CONFIG['development_mode']) {
+        $toMerge['DEVELOPMENT_MODE'] = true;
+    }
+    echo json_encode(array_merge([
+        'status' => 'error',
+        'message' => $message ?? __('Something went wrong, please try again later.')
+    ], $toMerge));
+}
+
+# Custom Error Handler
+function loggedErrorHandler($exception)
+{
+    if (IS_API_REQUEST) {
+        apiErrorResponse(null, [
+            'exception' => get_class($exception),
+            'message' => $exception->getMessage(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine()
+        ]);
+        die;
+    } else {
+        if (SYSTEM_CONFIG['development_mode']) {
+            echo "<b>DEVELOPMENT MODE</b><br><br>";
+            echo "<b>" . get_class($exception) . ":</b> " . $exception->getMessage();
+            echo "<br>File: " . $exception->getFile();
+            echo "<br>Line: " . $exception->getLine();
+            die;
+        }
+        require PAGES . '/error.php';
+    }
+    die;
+}
+set_exception_handler('loggedErrorHandler');
+
+# Prevents entry during maintenance
+if (SYSTEM_CONFIG['maintenance_mode'] && !isset($_REQUEST[SYSTEM_CONFIG['maintenance_key']])) {
+    if (IS_API_REQUEST) {
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'System is under maintenance. Please try again later'
+        ]);
+        die;
+    } else {
+        require PAGES . '/maintenance.php';
+        die;
+    }
+}
+
+# Block API requests during lockdown
+if (SYSTEM_CONFIG['lockdown_mode'] && IS_API_REQUEST) {
+    throw new Exception('System under lockdown mode');
+    die;
+}
+
+# Load configurations
+if (SYSTEM_CONFIG['development_mode']) {
+    // Always reparse the config
+    require 'config_parser.php';
+}
+$config = json_decode(file_get_contents(SYSTEM . '/app/cache/config.json'), true);
+if (!$config) {
+    require 'config_parser.php';
+    throw new Exception('Config is not initialized. If error persisted after refresh, config is malformated.');
+}
+define('CONFIG', Arr::dot($config));
+define('CONFIG_RAW', $config);
+unset($config);
+
+/**
+ * Config Function
+ * 
+ * @param string $key
+ * @param mixed $default
+ * 
+ * @return mixed
+ */
+function config(string $key, $default = null)
+{
+    if (isset(CONFIG[$key])) {
+        return CONFIG[$key];
+    }
+    $explode = explode('.', $key);
+    foreach ($explode as $index => $item) {
+        if ($index === 0) {
+            $current = CONFIG_RAW;
+        }
+        $current = $current[$item] ?? null;
+    }
+    if ($current !== null) {
+        return $current;
+    }
+    return $default;
+}
+
+# Load Language
+define('DICTIONARY', json_decode(file_get_contents(SYSTEM . '/language/' . config('system.language') . '.lang.json'), true));
+function __(string $key)
+{
+    return DICTIONARY[$key] ?? $key;
+}
+
+# Start session
+$toMerge = (SYSTEM_CONFIG['use_https']) ? ['cookie_secure' => true] : [];
+session_start(array_merge([
+    'cookie_httponly' => true,
+    'use_trans_sid' => false,
+    'gc_maxlifetime' => 7200
+], $toMerge));
+unset($toMerge);
+
+# Bootstrap Completed!
+# Ready to handle request
