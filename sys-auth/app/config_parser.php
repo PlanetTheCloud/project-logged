1dc9943a638a526e663f7a0c3909785aa771bc18
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2024-05-15 15:51:55 +0700

    Sync

--- sys-auth/app/config_parser.php
+++ sys-auth/app/config_parser.php
@@ -2,34 +2,39 @@
 
 namespace ConfigParser;
 
+use Arr;
 use InvalidConfigException;
+
+function checkRequiredParameters(array $config, array $required, string $name)
+{
+    foreach ($required as $key) {
+        if (!isset($config[$key])) {
+            throw new InvalidConfigException("Missing '{$key}' field in '{$name}' config.");
+        }
+    }
+}
 
 $config = [];
 
 # Handle Branding Config
 $branding = require SYSTEM . '/config/branding.php';
 $required = ['name', 'logo', 'slogan', 'main_website', 'favicon', 'accent_color', 'background_color', 'contact_email', 'report_abuse_email'];
-foreach ($required as $key) {
-    if (!isset($branding[$key])) {
-        throw new InvalidConfigException("Missing '{$key}' field in 'branding' config.");
-    }
-}
+checkRequiredParameters($branding, $required, 'branding');
 $config['branding'] = $branding;
 unset($branding, $required);
 
 # Handle System Config
 $system = require SYSTEM . '/config/system.php';
-$required = ['installation_url', 'development_mode', 'maintenance_mode', 'maintenance_key', 'lockdown_mode', 'language', 'use_https', 'cpanel_url', 'domain_selection', 'blacklisted_tld', 'default_plan', 'features'];
-foreach ($required as $key) {
-    if (!isset($system[$key])) {
-        throw new InvalidConfigException("Missing '{$key}' field in 'system' config.");
-    }
-}
+$required = ['installation_url', 'allow_external_request', 'stub_mode', 'development_mode', 'maintenance_mode', 'maintenance_key', 'lockdown_mode', 'language', 'use_https', 'cpanel_url', 'domain_selection', 'blacklisted_tld', 'default_plan', 'features'];
+checkRequiredParameters($system, $required, 'system');
 $config['system'] = $system;
 unset($system, $required);
 
 # Handle Network Config
-
+$network = require SYSTEM . '/config/network.php';
+$required = ['main_website', 'credentials'];
+checkRequiredParameters($network, $required, 'network');
+$config['network'] = Arr::only($network, ['main_website']);
 
 # Add timestamp
 $config['cached_on'] = time();
--- sys-auth/config/network.php
+++ sys-auth/config/network.php
@@ -1,18 +1,19 @@
 <?php
 
 /**
- * Signup Configuration
+ * Network Configuration
  * ---
  * Read the documentation for explainations.
  */
 
 return [
-    'main_site' => [
+    'main_website' => [
         'full_url' => 'https://main.com'
     ],
     'credentials' => [
         // Please read the documentation!
         [
+            'protocol' => 'https://',
             'domain' => 'example.com',
             'private_key' => 'you_are_at_risk_of_attack_by_not_reading_the_docs!'
         ],
