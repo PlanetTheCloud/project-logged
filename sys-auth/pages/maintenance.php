e91cdd219bc930d6edb9d8f113545164e0ac1932
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2024-05-15 22:37:18 +0700

    Fix typo, add clarity

--- sys-auth/pages/maintenance.php
+++ sys-auth/pages/maintenance.php
@@ -1,11 +1,11 @@
 <html>
     <head>
-        <title>Maintenance Mode</title>
+        <title>Under Maintenance</title>
     </head>
     <body>
         <h1>Maintenance Mode</h1>
-        <p>Sorry for the inconvinience. The authentication system are currently under maintenance.</p>
+        <p>Sorry for the inconvenience. We're currently performing maintenance on the authentication system..</p>
         <p>We are working hard to finish this as soon as possible. If you need any assistance please contact our support team.</p>
-        <p><a href="/">Go back to our website</a></p>
+        <p><a href="/">Go back to home page.</a></p>
     </body>
 </html>
